dafbe548251be5c222663e6ecd088932fd37a3e7
Svllenraa <saviradesti2812@gmail.com>
2026-01-06 17:21:21 +0700

    Add Category model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    public function up()
+{
+Schema::create('categories', function (Blueprint $table) {
+$table->id();
+$table->string('name');
+$table->text('description')->nullable();
+$table->timestamps();
+});
+}
+    use HasFactory;
+}
